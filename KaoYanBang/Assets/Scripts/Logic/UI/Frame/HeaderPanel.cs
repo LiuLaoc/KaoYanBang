36bfd4f771d12976a220e373eb1a346e803c377b
liulaoc <liulaoc@gmail.com>
2020-05-02 23:06:11 +0800

    个人界面

---
+++ KaoYanBang/Assets/Scripts/Logic/UI/Frame/HeaderPanel.cs
@@ -0,0 +1,20 @@
+﻿using liulaoc.UI.Base;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeaderPanel : UIPanel
+{
+    private Button backBtn;
+
+    protected override void AddListener()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void BindView()
+    {
+        backBtn = transform.Find("BackBtn").GetComponent<Button>();
+    }
+}

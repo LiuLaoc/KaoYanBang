fda496f255f7658d29cb013a436c774c3c721435
liulaoc <liulaoc@gmail.com>
2020-05-28 23:10:46 +0800

    咨询界面

--- KaoYanBang/Assets/Scripts/Logic/UI/Frame/InfoFrame/InfoPanel.cs
+++ KaoYanBang/Assets/Scripts/Logic/UI/Frame/InfoFrame/InfoPanel.cs
@@ -8,21 +8,48 @@ public class InfoPanel : UIPanel
 {
     #region view
     [SerializeField]private Toggle[] toogles;
+    [SerializeField]private GameObject PostPrefab;
+    [SerializeField]private GameObject InfoPrefab;
     private Transform group;
     private RectTransform groupTrans;
     #endregion
     #region model
-
+    private List<GameObject> AllPostList = new List<GameObject>();
+    private List<GameObject> AllInfoList = new List<GameObject>();
     #endregion
     protected override void AddListener()
     {
-        
+        toogles[0].onValueChanged.AddListener((value) =>
+        {
+            if(value)
+            {
+
+            }
+        });
+        toogles[1].onValueChanged.AddListener((value) =>
+        {
+            if (value)
+            {
+
+            }
+        });
+        toogles[2].onValueChanged.AddListener((value) =>
+        {
+            if (value)
+            {
+
+            }
+        });
     }
 
     protected override void BindView()
     {
         group = transform.Find("Scroll View").Find("Viewport").Find("HotTopicGroup");
         groupTrans = group.GetComponent<RectTransform>();
+        //加载所有Prefab
+    }
+    protected void UpdateView()
+    {
 
     }
 }

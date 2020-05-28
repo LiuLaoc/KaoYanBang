93be1eae4b650f535c4c171b14a73007428a77ae
liulaoc <liulaoc@gmail.com>
2020-05-28 22:27:58 +0800

    咨询页面修改，帖子模板修改

---
+++ KaoYanBang/Assets/Scripts/Logic/UI/Frame/InfoFrame/InfoPanel.cs
@@ -0,0 +1,28 @@
+﻿using liulaoc.UI.Base;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanel : UIPanel
+{
+    #region view
+    [SerializeField]private Toggle[] toogles;
+    private Transform group;
+    private RectTransform groupTrans;
+    #endregion
+    #region model
+
+    #endregion
+    protected override void AddListener()
+    {
+        
+    }
+
+    protected override void BindView()
+    {
+        group = transform.Find("Scroll View").Find("Viewport").Find("HotTopicGroup");
+        groupTrans = group.GetComponent<RectTransform>();
+
+    }
+}

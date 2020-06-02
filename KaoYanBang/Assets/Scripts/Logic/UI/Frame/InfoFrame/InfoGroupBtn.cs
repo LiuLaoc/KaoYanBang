76f1ea0e3abfc1da32d5cae2001c4703bfe52e91
liulaoc <liulaoc@gmail.com>
2020-06-03 00:02:09 +0800

    修改美术资源、完成表格基类、资讯页面跳转

---
+++ KaoYanBang/Assets/Scripts/Logic/UI/Frame/InfoFrame/InfoGroupBtn.cs
@@ -0,0 +1,42 @@
+﻿using liulaoc.UI.Base;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoGroupBtn : UIPanel
+{
+    #region view
+    private Button gradeBtn;
+    private Button reportBtn;
+    private Button subjectBtn;
+    private Button regradeBtn;
+    #endregion
+    protected override void AddListener()
+    {
+        gradeBtn.onClick.AddListener(() =>
+        {
+            UIMgr.Instance.CreateFrame("");
+        });
+        reportBtn.onClick.AddListener(() =>
+        {
+            UIMgr.Instance.CreateFrame("");
+        });
+        subjectBtn.onClick.AddListener(() =>
+        {
+            UIMgr.Instance.CreateFrame("");
+        });
+        regradeBtn.onClick.AddListener(() =>
+        {
+            UIMgr.Instance.CreateFrame("");
+        });
+    }
+
+    protected override void BindView()
+    {
+        gradeBtn = transform.Find("Grade").Find("GradeBtn").GetComponent<Button>();
+        reportBtn = transform.Find("Report").Find("ReportBtn").GetComponent<Button>();
+        subjectBtn = transform.Find("Subject").Find("SubjectBtn").GetComponent<Button>();
+        regradeBtn = transform.Find("ReGrade").Find("ReGradeBtn").GetComponent<Button>();
+    }
+}
---
+++ KaoYanBang/Assets/Scripts/Logic/UI/Frame/Preafab/TablePrefab.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TablePrefab : MonoBehaviour
+{
+    #region view
+    private RectTransform group;
+    #endregion
+    #region model
+    private List<>
+    #endregion
+    protected void Awake()
+    {
+        BindView();
+    }
+
+    protected void BindView()
+    {
+        group = transform.Find("TableView").Find("Viewport").Find("Content").GetComponent<RectTransform>();
+    }
+    protected void UpdateView()
+    {
+        //加载表格数据
+    }
+    protected void BindDataBase()
+    {
+
+    }
+}
